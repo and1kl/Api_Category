acfdc7c0a5aa235bc3e611715a38d0805f914093
and1kl <40032530+and1kl@users.noreply.github.com>
2019-11-12 18:33:33 +0100

    Spring Boot Project Initial

---
+++ Api_Category/src/main/java/de/hska/iwi/vslab/Api_Category/ApiCategoryApplication.java
@@ -0,0 +1,13 @@
+package de.hska.iwi.vslab.Api_Category;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiCategoryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiCategoryApplication.class, args);
+	}
+
+}
