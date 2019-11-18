83a2f750a0de1523ee732b0cf52838b25464c76b
and1kl <email.andreasklaiber@gmail.com>
2019-11-18 22:47:09 +0100

    Eureka Registrierung

--- Api_Category/src/main/java/de/hska/iwi/vslab/Api_Category/ApiCategoryApplication.java
+++ Api_Category/src/main/java/de/hska/iwi/vslab/Api_Category/ApiCategoryApplication.java
@@ -1,8 +1,12 @@
 package de.hska.iwi.vslab.Api_Category;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
+
+@EnableEurekaClient
 @SpringBootApplication
 public class ApiCategoryApplication {
 
